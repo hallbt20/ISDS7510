469e44321a697f4fbcd76c7cfa9891253f55921d
hallbt20 <62180184+hallbt20@users.noreply.github.com>
2023-06-03 12:37:19 -0500

    Update LAB4_Brad_Hall.sql

--- LAB4_Brad_Hall.sql
+++ LAB4_Brad_Hall.sql
@@ -18,28 +18,24 @@ SELECT UPPER(cName), capitalPop, IF(capitalPop > 500000, "High", "Low") AS popLe
 FROM country;
 
 # Part II – 1:1 and Recursive
-
-# 1) List monarch type, name, and number of the predecessor of King George VI.
 USE text;
 
+# 1) List monarch type, name, and number of the predecessor of King George VI.
 SELECT n.montype, m.premonname, m.premonnum
 FROM Monarch m, Monarch n
 WHERE (m.montype, m.monname, m.monnum) = (n.montype, n.monname, n.monnum) AND
 (m.monname, m.monnum) = ('George', 'VI');
 
 # 2) List monarch type, name, and number of the successor of Queen Victoria I.
-SELECT * FROM Monarch;
-
 SELECT n.montype, m.monname, m.monnum
 FROM Monarch m, Monarch n
 WHERE (m.montype, m.monname, m.monnum) = (n.montype, n.monname, n.monnum) AND
 (m.premonname, m.premonnum) = ('Victoria', 'I');
 
 # Part III – 1:M Recursive
-
-# 1) List the first names and salaries of employees who are a boss.
 USE text;
 
+# 1) List the first names and salaries of employees who are a boss.
 SELECT DISTINCT m.empfname, m.empsalary 
 FROM emp m, emp n
 WHERE m.empno = n.bossno;
@@ -50,5 +46,35 @@ FROM emp
 WHERE bossno = 1;
 
 # 3) For each boss, list the average salary of the employees who work for him or her. Include the first name of the boss.
+SELECT m.empfname AS bossName, 
+AVG(n.empsalary) AS avgEmpSalary
+FROM emp m, emp n
+WHERE m.empno = n.bossno
+GROUP BY n.bossno;
 
-SELECT * FROM emp;
+# Part IV – M:M Recursive
+USE patent;
+
+# 1) Find out which patents are citing patent No. 3992014.
+SELECT patentno, cited
+FROM citation
+WHERE cited = '3992014';
+
+# 2) Find out which patents are citing patent No. 3992014. List the category and applied year of the citing patents.
+SELECT c.patentno, p.category, p.yearofapply
+FROM citation c, patents p
+WHERE c.cited = '3992014'
+AND c.patentno = p.patent;
+
+# 3) Find out which patents are citing patent No. 3992014. List the category and applied year of both cited and citing patents.
+SELECT c.patentno AS citingPatentNo,
+p.category AS citingPatentCategory, 
+p.yearofapply AS citingPatentYearOfApply, 
+c.cited AS citedPatentNo,
+q.category AS citedPatentNo, 
+q.yearofapply AS citedPatentYearOfApply
+FROM citation c, citation d, patents p, patents q
+WHERE c.patentno = d.patentno
+AND c.patentno = p.patent
+AND d.cited = q.patent
+AND c.cited = '3992014';
